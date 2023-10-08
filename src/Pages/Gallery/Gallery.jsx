944b3937e82a7b50e4f9a83c76ef13427d0e56da
sristikundu1 <nidhukundu1234@gmail.com>
2023-10-09 04:29:19 +0600

    make the gallery page where people can see the image of our previous event

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -2,8 +2,16 @@
 
 const Gallery = () => {
     return (
-        <div>
-            gallery
+        <div className="bg-[#D3E0DC]">
+           <h2 className="font-bold text-4xl text-center text-[#086972] py-6">Memories</h2>
+           <div className="grid grid-cols-2">
+            <img src="https://i.ibb.co/wSLMYB9/decoration1.jpg" alt="" />
+            <img src="https://i.ibb.co/Tht5151/Wedding1.jpg" alt="" />
+            <img src="https://i.ibb.co/jHHTCF5/Retairement1.jpg" alt="" />
+            <img src="https://i.ibb.co/sPx7d44/Retirement2.jpg" alt="" />
+            <img src="https://i.ibb.co/tZSFhPr/Reception1.jpg" alt="" />
+            <img src="https://i.ibb.co/nQh1mZq/Wedding2.jpg" alt="" />
+           </div>
         </div>
     );
 };
